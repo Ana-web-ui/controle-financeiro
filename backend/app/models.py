61ab74a5ad77546a47d52eeb52eb8fa4ae521c37
Ana-web-ui <anacarolinaleite1401@gmail.com>
2026-02-13 20:31:12 -0300

    update: backend and fronted

--- backend/app/models.py
+++ backend/app/models.py
@@ -10,3 +10,11 @@ class Transaction(Base):
     categoria = Column(String, nullable=False)
     descricao = Column(String)
     data = Column(Date, default=date.today)
+    
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
